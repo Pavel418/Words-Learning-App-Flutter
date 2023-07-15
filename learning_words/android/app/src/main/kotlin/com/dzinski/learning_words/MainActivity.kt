65d79a53b313f40006addc05f7f2a57d26ee23e4
Pavel418 <pavel@vvv.az>
2023-07-15 18:18:45 +0400

    changed project template

---
+++ learning_words/android/app/src/main/kotlin/com/dzinski/learning_words/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dzinski.learning_words
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- words_learning_app/android/app/src/main/kotlin/com/dzinski/words_learning_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.dzinski.words_learning_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
